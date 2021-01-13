1ec125e07f4cefe522d3bc30e0a9e3d1c6473cae
FletchyAF <joshua.f1998@gmail.com>
2021-01-12 21:19:10 -0500

    commit tree 3824c281e37d8f39c710bdf48db0402ca3fb08bd

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
-def oxford_comma(array)
+require "pry"
 
+def oxford_comma(array)
+    if array.length == 1
+    return array.join
+    elsif array.length == 2
+    second_element = array[1]
+    array.pop
+    array << " and "
+    array << second_element
+    return array.join    
+    elsif array.length >= 3
+    last_element = array.last
+    array.pop
+    array << "and " + last_element
+    array.join(", ")
+    end
 end
